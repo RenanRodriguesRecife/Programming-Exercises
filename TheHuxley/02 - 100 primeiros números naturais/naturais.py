ff76815b7204f0b4a8c0401ffaae784f208e4c0c
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-11-09 21:00:16 -0300

    primeiro commit

---
+++ TheHuxley/02 - 100 primeiros números naturais/naturais.py
@@ -0,0 +1,17 @@
+#100 primeiros números naturais
+#
+#Descrição
+#Escreva um programa que exiba na saída padrão os 100 primeiros números naturais (inteiros positivos incluindo o zero).
+#
+#Formato de entrada
+#
+#Não há entrada.
+#
+#Formato de saída
+#
+#Os 100 primeiros números naturais (inteiros positivos incluindo o zero).
+
+i = 0
+while (i < 100):
+    print(i)
+    i += 1
